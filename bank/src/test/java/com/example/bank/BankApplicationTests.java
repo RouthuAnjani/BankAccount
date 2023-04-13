84e1795e3960e30d9fcf6a17b8938c6b37874c22
RouthuAnjani <130381213+RouthuAnjani@users.noreply.github.com>
2023-04-13 09:47:19 +0530

    Update BankApplicationTests.java

--- bank/src/test/java/com/example/bank/BankApplicationTests.java
+++ bank/src/test/java/com/example/bank/BankApplicationTests.java
@@ -1,13 +1,99 @@
 package com.example.bank;
 
+import com.example.bank.entity.BankAccount;
+import com.example.bank.repository.BankAccountRepository;
+import com.example.bank.service.BankService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class BankApplicationTests {
+	@InjectMocks
+	BankService bankService;
+	@Mock
+	BankAccountRepository bankAccountRepository;
 
 	@Test
-	void contextLoads() {
+	void testCreateAccount() {
+		BankAccount bankAccount = new BankAccount();
+		bankAccount.setAccountNumber("1234567890");
+		bankAccount.setAccountType("Checking");
+		bankAccount.setBalance(1000.0);
+		when(bankAccountRepository.save(any(BankAccount.class))).thenReturn(bankAccount);
+		BankAccount bankResponse = bankService.createBankAccount(bankAccount.getAccountNumber(),bankAccount.getAccountType(),bankAccount.getBalance());
 	}
 
+	@Test
+	void getBankAccountByAccountNumberTest() {
+		BankAccount bankAccount = new BankAccount();
+		bankAccount.setAccountNumber("1234567890");
+		bankAccount.setAccountType("Checking");
+		bankAccount.setBalance(1000.0);
+		when(bankAccountRepository.findByAccountNumber(anyString())).thenReturn(bankAccount);
+		BankAccount bankResponse = bankService.getBankAccountByAccountNumber(anyString());
+	}
+	@Test
+	void getAllBankAccountsTest() {
+		BankAccount bankAccount = new BankAccount();
+		bankAccount.setAccountNumber("1234567890");
+		bankAccount.setAccountType("Checking");
+		bankAccount.setBalance(1000.0);
+		when(bankAccountRepository.findAll()).thenReturn(List.of(bankAccount));
+		List<BankAccount> accounts =bankService.getAllBankAccounts();
+	}
+//	@Test
+//	void getBankAccountByAccountNumberNullTest() {
+//		BankAccount bankAccount = new BankAccount();
+//		bankAccount.setAccountNumber("1234567890");
+//		bankAccount.setAccountType("Checking");
+//		bankAccount.setBalance(1000.0);
+//		when(bankAccountRepository.findByAccountNumber(anyString())).thenReturn(null);
+//		when(bankService.getBankAccountByAccountNumber(anyString())).thenThrow(RuntimeException.class);
+//	}
+	@Test
+	void testUpdateAccount() {
+		BankAccount bankAccount = new BankAccount();
+		bankAccount.setAccountNumber("1234567890");
+		bankAccount.setAccountType("Checking");
+		bankAccount.setBalance(1000.0);
+		when(bankAccountRepository.findByAccountNumber(anyString())).thenReturn(bankAccount);
+		when(bankAccountRepository.save(bankAccount)).thenReturn(bankAccount);
+		BankAccount bankResponse = bankService.updateBankAccount(bankAccount.getAccountNumber(),bankAccount.getAccountType(),bankAccount.getBalance());
+
+	}
+	@Test
+	void findByAccountNumberTest() {
+		BankAccount bankAccount = new BankAccount();
+		bankAccount.setAccountNumber("1234567890");
+		bankAccount.setAccountType("Checking");
+		bankAccount.setBalance(1000.0);
+		when(bankAccountRepository.findByAccountNumber(anyString())).thenReturn(bankAccount);
+		BankAccount bankResponse = bankService.findByAccountNumber(bankAccount.getAccountNumber());
+
+	}
+	@Test
+	void deleteById() {
+		BankAccount bankAccount = new BankAccount();
+		bankAccount.setAccountNumber("1234567890");
+		bankAccount.setAccountType("Checking");
+		bankAccount.setBalance(1000.0);
+		when(bankAccountRepository.findByAccountNumber(anyString())).thenReturn(bankAccount);
+		bankService.deleteById(bankAccount.getAccountNumber());
+
+	}
 }
+
